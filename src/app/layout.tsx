486318f41933f465c317ac0c713f5538896e5279
igor297 <igor297297@gmail.com>
2025-06-30 14:40:38 -0300

    Atualizações no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClientShell>
-          {children}
-        </ClientShell>
+        {children}
       </body>
     </html>
   );
